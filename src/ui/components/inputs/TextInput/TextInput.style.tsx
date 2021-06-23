41aa8ff399bf39165d914ce3aa27adb7d9ceb6e6
ThierryNunes <70761860+ThierryNunes@users.noreply.github.com>
2021-06-23 16:18:58 -0300

    Add files via upload

---
+++ src/ui/components/inputs/TextInput/TextInput.style.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/native';
+import { TextInput } from 'react-native-paper';
+
+export const TextInputStyled = styled(TextInput)``;
+
+TextInput.defaultProps = {
+    mode: 'outlined',
+}
